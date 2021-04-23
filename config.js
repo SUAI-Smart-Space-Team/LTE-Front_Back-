2d19affee7a7e26fbbd791ac8ce662b4e9aa65de
Rachugin Roman <79422421+rro1699@users.noreply.github.com>
2021-04-23 19:12:41 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,16 +5,4 @@ module.exports = {
 	database: "mybd",
 	port: "3306"
 }
-/*
-host: "localhost",
-	user: "user",
-	password: "1234",
-	database: "mybd",
-	port: "3306"
-	
-	host: "us-cdbr-east-03.cleardb.com",
-	user: "b1eeb56c3c15f6",
-	password: "e953f37a",
-	database: "heroku_00aeb0b4dc34359",
-	port: "3306"
-"*/
+
